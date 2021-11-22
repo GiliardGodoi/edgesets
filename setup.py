4778eaec5bf46ce62d85314578e74c66f0b573c9
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-11-21 21:08:18 -0300

    minor updates

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="edgesets",
-    version='0.0.1',
+    version='0.0.0',
+    author='@GiliardGodoi',
+    author_email='giliard.godoi@gmail.com',
     description="A tree representation for Evolutionary Algorithms",
     long_description=open('README.md').read(),
-    author='Giliard Godoi',
-    author_email='giliard.godoi@gmail.com',
     keywords=['Edge Sets', 'Tree representation', 'Evolutionary Algorithms'],
     url='https://github.com/GiliardGodoi/edgesets',
     license='MIT License',
