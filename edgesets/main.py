49fc6c858df08e38c91dc07223f4e101cf41ce82
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-11-21 22:02:15 -0300

    minor fixes: update edgesets main

--- edgesets/main.py
+++ edgesets/main.py
@@ -1,21 +1,19 @@
 
 import operator
 import functools
-
-from typing import Generator
+import reprlib
 
 class GEdge:
     '''General Edge Abrastraction'''
     def __init__(self, u, v, weight=1, **kwargs):
-        self._edge = (u, v)
-        self._weight = weight
+        self._edge = (u, v, weight)
 
     def __str__(self):
         return repr(self)
 
     def __repr__(self):
         cls = type(self).__name__
-        return f"{cls}{self._edge}"
+        return "{}({!r}, {!r}, weight={!r})".format(cls,*self._edge)
 
     def __hash__(self):
         cls = type(self).__name__
@@ -23,37 +21,40 @@ class GEdge:
         return functools.reduce(operator.xor, hashes, 0)
 
     def __contains__(self, vertice):
-        return vertice in self._edge
+        return (vertice == self._edge[0]
+                or vertice == self._edge[1])
 
     def __iter__(self):
-        return iter(self._edge)
+        yield self._edge[0]
+        yield self._edge[1]
 
-    def __getitem__(self, key):
-        return self._edge[key]
+    def __getitem__(self, index):
+        return self._edge[index]
 
     def __len__(self):
-        return len(self._edge)
+        return len(self._edge) - 1
 
     def __abs__(self):
-        return self._weight
+        return self.weight
 
     def __eq__(self, other):
         return (type(self) == type(other) and
-                    self._edge == other._edge and
-                    self.weight == other.weight )
+                    self._edge == other._edge )
+                    # comparing weight sounds reduntant now
+                    # and self.weight == other.weight )
 
     @property
     def vertices(self):
-        for v in self._edge:
-            yield v
+        for i in [0, 1]:
+            yield self._edge[i]
 
     @property
     def weight(self):
-        return self._weight
+        return self._edge[2]
 
     @property
     def w(self):
-        return self._weight
+        return self.weight
 
 
 class UEdge(GEdge):
@@ -73,24 +74,38 @@ class DEdge(GEdge):
 
 class EdgeSet:
 
-    def __init__(self,items=None):
+    @classmethod
+    def from_iter(cls, items, directed=False):
+
+        def weight(e):
+            if len(e) == 3:
+                return e[2]
+            elif len(e) == 2:
+                return 1
+            else:
+                raise TypeError("items should have 2 arguments for node or at least one for weight")
+
+        Edge = DEdge if directed else UEdge
+        result = cls()
+
+        for e in items:
+            result.add(Edge(e[0], e[1], weight=weight(e)))
+
+        return result
+
+    def __init__(self):
         self._edges = set()
-
-        if items and isinstance(items, Generator):
-            items = [item for item in items]
-
-        if items and isinstance(items, (list, tuple, set)):
-            if not all(issubclass(item, GEdge) for item in items):
-                raise AttributeError("Some itens are not Edges")
-            self._edges = set(items)
-        elif items and isinstance(items, self.__class__):
-            self._edges = items._edges.copy()
+        self._etype = None
 
     def __str__(self):
-        return f'EdgeSet: <{len(self)}>'
+        return repr(self)
 
     def __repr__(self):
-        return f'EdgeSet: <{len(self)}>'
+        class_name = type(self).__name__
+        if not self._edges:
+            return "{}()".format(class_name)
+        components = reprlib.repr(self._edges)
+        return "{}({})".format(class_name, components)
 
     def __contains__(self, item):
         return item in self._edges
@@ -101,35 +116,40 @@ class EdgeSet:
     def __len__(self):
         return len(self._edges)
 
+    def __bool__(self):
+        return bool(self._edges)
+
     def __sub__(self, other):
         '''- operator'''
         if not isinstance(other, EdgeSet):
-            raise TypeError(f"unsupported operand type(s) for -: '{type(self)}' and {type(other)}")
+            raise TypeError(f"unsupported operand type(s) for - '{type(self)}' and {type(other)}")
         return EdgeSet(self._edges - other._edges)
 
     def __and__(self, other):
         '''& operator'''
         if not isinstance(other, EdgeSet):
-            raise TypeError(f"unsupported operand type(s) for &: '{type(self)}' and {type(other)}")
+            raise TypeError(f"unsupported operand type(s) for & '{type(self)}' and {type(other)}")
         return EdgeSet(self._edges & other._edges)
 
     def __xor__(self, other):
         '''^ operator'''
         if not isinstance(other, EdgeSet):
-            raise TypeError(f"unsupported operand type(s) for ^: '{type(self)}' and {type(other)}")
+            raise TypeError(f"unsupported operand type(s) for ^ '{type(self)}' and {type(other)}")
         return EdgeSet(self._edges ^ other._edges)
 
     def __or__(self, other):
         ''' | operator'''
         if not isinstance(other, EdgeSet):
-            raise TypeError(f"unsupported operand type(s) for -: '{type(self)}' and {type(other)}")
+            raise TypeError(f"unsupported operand type(s) for | '{type(self)}' and {type(other)}")
         return EdgeSet(self._edges | other._edges)
 
     def __eq__(self, other):
         '''== operator '''
         if not isinstance(other, EdgeSet):
-            raise TypeError(f"unsupported operand type(s) for ==: '{type(self)}' and {type(other)}")
-        return self._edges == other._edges
+            raise TypeError(f"unsupported operand type(s) for == '{type(self)}' and {type(other)}")
+        return (type(self) == type(other)
+                    and len(self) == len(other)
+                    and all( e in self for e in other))
 
     def __lt__(self, other):
         '''< operator '''
@@ -140,19 +160,19 @@ class EdgeSet:
     def __le__(self, other):
         '''<= operator '''
         if not isinstance(other, EdgeSet):
-            raise TypeError(f"unsupported operand type(s) <=  for '{type(self)}' and {type(other)}")
+            raise TypeError(f"unsupported operand type(s) for <= '{type(self)}' and {type(other)}")
         return self._edges <= other._edges
 
     def __gt__(self, other):
         '''> operator '''
         if not isinstance(other, EdgeSet):
-            raise TypeError(f"unsupported operand type(s) > for '{type(self)}' and {type(other)}")
+            raise TypeError(f"unsupported operand type(s) for > '{type(self)}' and {type(other)}")
         return self._edges > other._edges
 
     def __ge__(self, other):
         '''>= operator '''
         if not isinstance(other, EdgeSet):
-            raise TypeError(f"unsupported operand type(s) >= for '{type(self)}' and {type(other)}")
+            raise TypeError(f"unsupported operand type(s) for >= '{type(self)}' and {type(other)}")
         return self._edges >= other._edges
 
     @property
@@ -168,21 +188,32 @@ class EdgeSet:
         if issubclass(type(param), GEdge):
             return param
         else:
-            raise ValueError(f"could not understand input f{param}")
-
-        return UEdge(u, v)
+            raise TypeError(f"could not understand input f{param}")
 
     def add(self, edge):
         edge = self.__check_args(edge)
+        if self._etype is None:
+            self._etype = type(edge)
+
+        if type(edge) != self._etype:
+            raise TypeError('EdgeSet should have the same type of edges')
+
         self._edges.add(edge)
 
-    def discard(self, *args):
-        edge = self.__check_args(args)
-        self._edges.discard(edge)
+    def pop(self):
+        return self._edges.pop()
 
-    def remove(self, *args):
-        edge = self.__check_args(args)
-        self._edges.remove(edge)
+    def discard(self, edge):
+        if type(edge) == self._etype:
+            self._edges.discard(edge)
+        else:
+            raise TypeError(f"Given edge is not the same type of edges in this EdgeSet. Type: {type(edge)}")
+
+    def remove(self, edge):
+        if type(edge) == self._etype:
+            self._edges.remove(edge)
+        else:
+            raise TypeError(f"Given edge is not the same type of edges in this EdgeSet. Type: {type(edge)}")
 
     def issubset(self, other):
         if not isinstance(other, EdgeSet):
@@ -202,5 +233,6 @@ class EdgeSet:
         result._edges = self._edges.copy()
         return result
 
-    def pop(self):
-        return self._edges.pop()
+    def is_empty(self):
+        '''Return True if the EdgeSet is empty'''
+        return not bool(self)
